41ded7fead66ed5d04709e80d4cb1b976f9dd2ac
Tiziano <costantinitiziano05@gmail.com>
2026-08-28 12:45:30 -0300

    Refactorizacion del codigo CSS Y JS

--- js/constantes.js
+++ js/constantes.js
@@ -1,12 +1,23 @@
+/* =========================================
+   DATOS GLOBALES DE LA COOPERATIVA
+   ========================================= */
+
 const MAYCOOP_DATOS = {
+    // Teléfonos
     telComercial: "11 3484-3630",
     telCobranzas: "11 4080-7753",
+
+    // Correos
     email: "info.maycoop@gmail.com",
+
+    // Direcciones
     direccionCABA: "25 de Mayo 267 piso 3 oficina 310, CABA",
     direccionHaedo: "Constitución 317, Haedo",
+
+    // Redes Sociales
     instagram: "maycoopok",
 
-    // Variables dinámicas
+    // Estadisticas de la Cooperativa
     anioFundacion: 2010,
     totalSocios: "13.000",
     totalCreditos: "20.000"
